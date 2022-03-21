14c5ab9f63e60a8925d6d0eee057b2aaa379f007
Kim G <xiaole.guo@yahoo.com>
2022-03-21 16:56:36 -0600

    Patients validates the presence of all columns and belongs to dentist

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,3 +1,4 @@
 class Patient  < ApplicationRecord
   belongs_to :dentist
+  validates_presence_of :name, :created_at, :updated_at, :age, :dentist_id, :referred_by, :referred_by_another_patient, :insurance_carrier
 end
---
+++ spec/models/patient_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe Patient, type: :model do
+
+  describe 'validations' do
+    it {should validate_presence_of :name}
+    it {should validate_presence_of :created_at}
+    it {should validate_presence_of :updated_at}
+    it {should validate_presence_of :age}
+    it {should validate_presence_of :dentist_id}
+    it {should validate_presence_of :referred_by}
+    it {should validate_presence_of :referred_by_another_patient}
+    it {should validate_presence_of :insurance_carrier}
+  end
+
+  describe 'relationships' do
+    it {should belong_to :dentist}
+
+  end
+
+end
