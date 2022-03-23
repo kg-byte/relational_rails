4d1d1fd292c2d59f8a242c92743d34c993b06669
Kim G <xiaole.guo@yahoo.com>
2022-03-23 15:01:04 -0600

    add Dentist model method order_by_created_at

--- app/models/dentist.rb
+++ app/models/dentist.rb
@@ -1,4 +1,8 @@
 class Dentist  < ApplicationRecord
   has_many :patients
   validates_presence_of :name, :max_patient_capacity, :accepting_new_patient, :hours, :rating
+
+  def self.orer_by_created_at
+    self.all.order(created_at: :desc)
+  end 
 end
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,4 +1,14 @@
 class Patient  < ApplicationRecord
   belongs_to :dentist
   validates_presence_of :name, :age, :insurance_carrier
+
+
+  def self.referred_by?
+
+    if referred_by_anotehr_patient != nil 
+      referred_by = true 
+    else 
+      false
+    end 
+  end 
 end
